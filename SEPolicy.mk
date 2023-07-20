0001eac75e001b34b9ff7c2e8cc52c991d8eb35e
Giovanni Ricca <giovanniricca@proton.me>
2023-07-20 19:54:10 +0200

    sepolicy: lineage: Label our Livedisplay hidl service
    Change-Id: I4396fc2cbcedebda61a489763b5374425e2b7847

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -5,7 +5,9 @@ SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     $(SEPOLICY_PATH)/common/private
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    $(SEPOLICY_PATH)/common/public
+    $(SEPOLICY_PATH)/common/public \
+    $(SEPOLICY_PATH)/lineage/public
 
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    $(SEPOLICY_PATH)/common/vendor
+    $(SEPOLICY_PATH)/common/vendor \
+    $(SEPOLICY_PATH)/lineage/vendor
